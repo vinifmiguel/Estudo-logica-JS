60c2b6db16571f207306e3edd0735c1cf192b462
Vinicius F Miguel <vinifmiguel2@gmail.com>
2025-08-28 19:16:30 -0300

    Adicionando pasta do treinamento dia 3

---
+++ Dia 3/index.js
@@ -0,0 +1,13 @@
+let tipotexto = "Olá "
+let tipotexto2 = "Mundo!"
+let tiponumber1 = 5
+let tiponumber2 = 10
+let tipobool1 = true
+let tipobool2 = false
+
+console.log(tipotexto+tipotexto2)
+
+console.log(tiponumber1+tiponumber2)
+console.log(tiponumber2/tiponumber1)
+console.log(tiponumber1*tiponumber2)
+
